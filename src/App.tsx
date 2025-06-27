32baa5ea68f99097fe6a629b4ce8c14f3156ec04
Ans de Nijs <ans@chordify.net>
2025-06-27 10:44:09 +0200

    Fix: Install the packages correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,26 @@
-// import { Provider } from '@/components/ui/provider'
-import { Map as MapLibreMap } from '@vis.gl/react-maplibre'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import { Marker, default as ReactMapGL } from 'react-map-gl/maplibre'
 import './App.css'
-
-import 'maplibre-gl/dist/maplibre-gl.css'
+import { Provider } from './components/ui/provider'
 
 function App() {
 
   return (
-    <>
-      <MapLibreMap
-      initialViewState={{
-          longitude: -100,
-          latitude: 40,
-          zoom: 3.5
-        }}
-        style={{width: 600, height: 400}}
-        mapStyle="https://demotiles.maplibre.org/style.json"
-      />
+    <Provider>
       <div>This is a map</div>
-      {/* <Map
+      <ReactMapGL
       initialViewState={{
         latitude: 37.8,
         longitude: -122.4,
         zoom: 14
       }}
-      mapLib={maplibregl}
+      // mapLib={maplibregl}
       style={{width: 800, height: 600}}
       mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
     >
       <Marker longitude={-122.4} latitude={37.8} color="red" />
-    </Map> */}
-    </>
+    </ReactMapGL>
+    </Provider>
   )
 }
 
